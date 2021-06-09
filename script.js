8d009bab76cceefc6cc4374e9de1bd7c5e9afcb8
Matt Dudzik <mdudzik92@gmail.com>
2021-06-09 17:09:51 -0500

    Connecting the JS to the HTML

---
+++ script.js
@@ -0,0 +1,4 @@
+// Setting up the variables for traversing the DOM
+var tipEl = document.querySelector("#tip-percentage");
+var totalEl = document.querySelector("#total");
+var submitEl = document.querySelector("#submit");
