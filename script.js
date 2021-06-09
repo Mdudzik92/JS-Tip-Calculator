b7d66278675e8b99e0a7c144bfcd8be081eac6c6
Matt Dudzik <mdudzik92@gmail.com>
2021-06-09 17:38:30 -0500

    Adding function that calculates the tip and prints it to the DOM

--- script.js
+++ script.js
@@ -2,3 +2,25 @@
 var tipEl = document.querySelector("#tip-percentage");
 var totalEl = document.querySelector("#total");
 var submitEl = document.querySelector("#submit");
+
+// Calculating the tip using a function to multiply the tip by the percentage, turned into a decimal.
+function calculateTip(total, tipPercentage) {
+	// Multiplying the two, to two decimal places using the toFixed method.
+	var roundedResult = (total * tipPercentage).toFixed(2);
+	return roundedResult;
+}
+
+// Add the tip to the h4 in the markup.
+function addTip(event) {
+	event.preventDefault();
+	// Turning the value of the entered tip into a decimal for multiplication.
+	var tipPercentage = tipEl.value * 0.01;
+	var total = totalEl.value;
+	// Defining what multiplying the two end up as into one variable.
+	var tipAmount = calculateTip(total, tipPercentage);
+	// Printing the new variable to the DOM.
+	document.querySelector("#tip-amount").textContent = tipAmount;
+}
+
+// Set the onclick event to put the new total in the div below the form in the markup.
+submitEl.addEventListener("click", addTip);
